ec10e567e1f31db5a962c6408f0319b4776dc051
chandru-md <chandrumd2071@gmail.com>
2026-07-30 17:01:38 +0530

    Comments section of python

--- comments.py
+++ comments.py
@@ -3,3 +3,11 @@ first_name = "Chandru" #first name of the user
 last_name = "M D" # last name of the user
 
 user_age = 24 # age of the user
+
+# Comment
+
+## This is the comment in Python
+
+"""
+this is multi line comment in python
+"""
